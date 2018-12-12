eaf5f87f8f0c60f9526e8f1fa070f014787bb4ea
Kalin Chernev <kalin.chernev@gmail.com>
2018-12-12 12:20:44 +0200

    Use ECL 2.0

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,6 @@
-// Theme specific assets
+// Styles
+import "@ecl/ec-preset-website/dist/styles/ecl-ec-preset-website.css";
 import "./styles/github.scss";
+
+// JavaScript
 import "./js/site";
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.scss$/,
+        test: /\.(scss$|css)$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
           use: [
